02df2f856085a746543d4858f8abc2f87d7c9c53
Nathan Yang <nathan.yang@usask.ca>
2021-03-18 11:35:56 +0800

    move config.go and remove the IPFS field

--- config/config.go
+++ config/config.go
@@ -1,10 +1,9 @@
-package main
+package config
 
 import "time"
 
 type Config struct {
 	FFmpeg         string        `json:"ffmpeg"`
-	IPFS           string        `json:"ipfs"`
 	SamplesPath    string        `json:"samples_path"`
 	SampleDuration time.Duration `json:"sample_duration"`
 }
